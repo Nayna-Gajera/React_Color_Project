1277df2bbbdc6f7b10999307ee196b63143c3e56
Nayna Gajera <nayna.gajera21@gmail.com>
2021-04-19 23:41:54 -0700

    Updates

---
+++ precache-manifest.f01b84395802b978febda32361eea632.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e2df13e709f4c66fd0c",
+    "url": "/React_Color_Project/static/css/main.472dc846.chunk.css"
+  },
+  {
+    "revision": "1e2df13e709f4c66fd0c",
+    "url": "/React_Color_Project/static/js/main.2013f698.chunk.js"
+  },
+  {
+    "revision": "dac8a774394b174f0c86",
+    "url": "/React_Color_Project/static/js/runtime~main.c83c7e0e.js"
+  },
+  {
+    "revision": "7ad7b7eefdc6fce7edc1",
+    "url": "/React_Color_Project/static/css/2.be983833.chunk.css"
+  },
+  {
+    "revision": "7ad7b7eefdc6fce7edc1",
+    "url": "/React_Color_Project/static/js/2.c439ba46.chunk.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/React_Color_Project/static/media/bg.77428cd4.svg"
+  },
+  {
+    "revision": "7c6d345801e73f08c31dc653b27aa27b",
+    "url": "/React_Color_Project/index.html"
+  }
+];
